3c9f629642a2469e4bdd053ba93113d1055fd397
Julien Coulon <julien.c.git@gmail.com>
2022-09-09 09:51:19 +0200

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+
+import Options from "./components/options";
+import logo from "./logo.svg";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="absolute top-0 right-0 ">
+        <Options />
+      </div>
+      <h1 className="bg-red-100 text-3xl font-bold text-center p-8">
+        My React App
+      </h1>
+      <section>
+        <img className="mx-auto p-24" src={logo} alt="logo" width="800" />
+      </section>
+    </>
   );
 }
-
-export default App;
